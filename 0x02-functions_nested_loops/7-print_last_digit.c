d12b6e7f08ae15ac79dbc10e80484c5822bd5df8
AbdelazizNaSSeR <zezosabek66@gmail.com>
2023-08-15 18:48:39 -0700

    All_Tasks

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,6 +7,7 @@
 int print_last_digit(int n)
 {
 	int a;	
+
 	if (n < 0)
 		n = -n;
 
@@ -14,8 +15,9 @@ int print_last_digit(int n)
 		
 	if (a < 0)
 		a = -a;
-
+	
 	_putchar(a + '0');
 	
-	return (a);	
+	return (a);
+		
 }
